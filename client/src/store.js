b3f6a70a0f9ec9a5b177b0f4622457b817048cb2
Chrissy Zwainz <zchrissychrissy@gmail.com>
2019-03-13 10:03:38 -0600

    drag n drop installed

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { Drag, Drop } from 'vue-drag-drop';
+
 
 Vue.use(Vuex)
+Vue.component('drag', Drag);
+Vue.component('drop', Drop);
 
 export default new Vuex.Store({
   state: {
